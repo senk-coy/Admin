9cbce66d529b238e1fc617fb6ebce2f187f3a46d
ngixc1871 <68748197+ngixc1871@users.noreply.github.com>
2020-07-25 00:08:40 +0700

    Create redi.js

---
+++ redi.js
@@ -0,0 +1,13 @@
+var url = "./login"; // url tujuan
+var count = 3; // dalam detik
+function countDown() {
+    if (count > 0) {
+        count--;
+        var waktu = count + 1;
+        $('#kata').html('<b>Halaman Ini Akan Otomatis Di Redirect KE </b> ' + url + ' dalam ' + waktu + ' detik.');
+        setTimeout("countDown()", 1000);
+    } else {
+        window.location.href = url;
+    }
+}
+countDown();
